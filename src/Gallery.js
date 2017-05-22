68c64d8268ded666868997f45dccafc5c89470cb
Jeremy Buttice <jeremy@tymate.com>
2017-05-22 16:44:01 +0200

    [Gallery] Get lightbox isOpen state from prevProps

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,10 +1,10 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import Lightbox from 'react-images';
-import Image from './Image.js';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import Lightbox from "react-images";
+import Image from "./Image.js";
 
 class Gallery extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -24,30 +24,34 @@ class Gallery extends Component {
         this.onSelectImage = this.onSelectImage.bind(this);
     }
 
-    componentDidMount () {
+    componentDidMount() {
         this.onResize();
-        window.addEventListener('resize', this.onResize);
+        window.addEventListener("resize", this.onResize);
     }
 
-    componentWillReceiveProps (np) {
-        if(this.state.images != np.images){
+    componentWillReceiveProps(np) {
+        if (this.state.images != np.images) {
             this.setState({
                 images: np.images,
-                thumbnails: this.renderThumbs(this._gallery.clientWidth,
-                                              np.images)
+                thumbnails: this.renderThumbs(
+                    this._gallery.clientWidth,
+                    np.images
+                )
             });
         }
     }
 
-    componentDidUpdate () {
+    componentDidUpdate(prevProps, prevState) {
+        if (!prevProps.isOpen && this.props.isOpen) {
+            this.openLightbox(this.props.currentImage);
+        }
         if (!this._gallery) return;
-        if (this._gallery.clientWidth
-            !== this.state.containerWidth){
+        if (this._gallery.clientWidth !== this.state.containerWidth) {
             this.onResize();
         }
     }
 
-    onResize () {
+    onResize() {
         if (!this._gallery) return;
         this.setState({
             containerWidth: Math.floor(this._gallery.clientWidth),
@@ -55,8 +59,8 @@ class Gallery extends Component {
         });
     }
 
-    openLightbox (index, event) {
-        event.preventDefault();
+    openLightbox(index, event) {
+        event && event.preventDefault();
         if (this.props.lightboxWillOpen) {
             this.props.lightboxWillOpen(index);
         }
@@ -67,7 +71,7 @@ class Gallery extends Component {
         });
     }
 
-    closeLightbox () {
+    closeLightbox() {
         if (this.props.lightboxWillClose) {
             this.props.lightboxWillClose();
         }
@@ -78,60 +82,55 @@ class Gallery extends Component {
         });
     }
 
-    gotoPrevious () {
+    gotoPrevious() {
         this.setState({
             currentImage: this.state.currentImage - 1
         });
     }
 
-    gotoNext () {
+    gotoNext() {
         this.setState({
             currentImage: this.state.currentImage + 1
         });
     }
 
-    onClickImage () {
-        if (this.state.currentImage === this.props.images.length - 1)
-            return;
+    onClickImage() {
+        if (this.state.currentImage === this.props.images.length - 1) return;
         this.gotoNext();
     }
 
-    onSelectImage (index, event) {
+    onSelectImage(index, event) {
         event.preventDefault();
-        if(this.props.onSelectImage)
+        if (this.props.onSelectImage)
             this.props.onSelectImage(index, this.state.images[index]);
     }
 
-    getOnClickThumbnailFn () {
-        if(!this.props.onClickThumbnail && this.props.enableLightbox)
+    getOnClickThumbnailFn(idx) {
+        if (!this.props.onClickThumbnail && this.props.enableLightbox)
             return this.openLightbox;
-        if(this.props.onClickThumbnail)
-            return this.props.onClickThumbnail;
+        if (this.props.onClickThumbnail) return this.props.onClickThumbnail;
         return null;
     }
 
-    getOnClickImageFn () {
-        if(this.props.onClickImage)
-            return this.props.onClickImage;
+    getOnClickImageFn() {
+        if (this.props.onClickImage) return this.props.onClickImage;
         return this.onClickImage;
     }
 
-    getOnClickPrevFn () {
-        if(this.props.onClickPrev)
-            return this.props.onClickPrev;
+    getOnClickPrevFn() {
+        if (this.props.onClickPrev) return this.props.onClickPrev;
         return this.gotoPrevious;
     }
 
-    getOnClickNextFn () {
-        if(this.props.onClickNext)
-            return this.props.onClickNext;
+    getOnClickNextFn() {
+        if (this.props.onClickNext) return this.props.onClickNext;
         return this.gotoNext;
     }
 
-    calculateCutOff (len, delta, items) {
+    calculateCutOff(len, delta, items) {
         var cutoff = [];
         var cutsum = 0;
-        for(var i in items) {
+        for (var i in items) {
             var item = items[i];
             var fractOfLen = item.scaletwidth / len;
             cutoff[i] = Math.floor(fractOfLen * delta);
@@ -139,8 +138,8 @@ class Gallery extends Component {
         }
 
         var stillToCutOff = delta - cutsum;
-        while(stillToCutOff > 0) {
-            for(i in cutoff) {
+        while (stillToCutOff > 0) {
+            for (i in cutoff) {
                 cutoff[i]++;
                 stillToCutOff--;
                 if (stillToCutOff < 0) break;
@@ -149,28 +148,27 @@ class Gallery extends Component {
         return cutoff;
     }
 
-    buildImageRow (items, containerWidth) {
+    buildImageRow(items, containerWidth) {
         var row = [];
         var len = 0;
         var imgMargin = 2 * this.props.margin;
-        while(items.length > 0 && len < containerWidth) {
+        while (items.length > 0 && len < containerWidth) {
             var item = items.shift();
             row.push(item);
-            len += (item.scaletwidth + imgMargin);
+            len += item.scaletwidth + imgMargin;
         }
 
         var delta = len - containerWidth;
-        if(row.length > 0 && delta > 0) {
+        if (row.length > 0 && delta > 0) {
             var cutoff = this.calculateCutOff(len, delta, row);
-            for(var i in row) {
+            for (var i in row) {
                 var pixelsToRemove = cutoff[i];
                 item = row[i];
                 item.marginLeft = -Math.abs(Math.floor(pixelsToRemove / 2));
                 item.vwidth = item.scaletwidth - pixelsToRemove;
             }
-        }
-        else {
-            for(var j in row) {
+        } else {
+            for (var j in row) {
                 item = row[j];
                 item.marginLeft = 0;
                 item.vwidth = item.scaletwidth;
@@ -179,13 +177,13 @@ class Gallery extends Component {
         return row;
     }
 
-    setThumbScale (item) {
-        item.scaletwidth =
-            Math.floor(this.props.rowHeight
-                       * (item.thumbnailWidth / item.thumbnailHeight));
+    setThumbScale(item) {
+        item.scaletwidth = Math.floor(
+            this.props.rowHeight * (item.thumbnailWidth / item.thumbnailHeight)
+        );
     }
 
-    renderThumbs (containerWidth, images = this.state.images) {
+    renderThumbs(containerWidth, images = this.state.images) {
         if (!images) return [];
         if (containerWidth == 0) return [];
 
@@ -196,19 +194,18 @@ class Gallery extends Component {
 
         var thumbs = [];
         var rows = [];
-        while(items.length > 0) {
+        while (items.length > 0) {
             rows.push(this.buildImageRow(items, containerWidth));
         }
 
-        for(var r in rows) {
-            for(var i in rows[r]) {
+        for (var r in rows) {
+            for (var i in rows[r]) {
                 var item = rows[r][i];
-                if(this.props.maxRows) {
-                    if(r < this.props.maxRows) {
+                if (this.props.maxRows) {
+                    if (r < this.props.maxRows) {
                         thumbs.push(item);
                     }
-                }
-                else {
+                } else {
                     thumbs.push(item);
                 }
             }
@@ -216,50 +213,54 @@ class Gallery extends Component {
         return thumbs;
     }
 
-    render () {
+    render() {
         var images = this.state.thumbnails.map((item, idx) => {
-            return <Image
-            key={"Image-"+idx+"-"+item.src}
-            item={item}
-            index={idx}
-            margin={this.props.margin}
-            height={this.props.rowHeight}
-            isSelectable={this.props.enableImageSelection}
-            onClick={this.getOnClickThumbnailFn()}
-            onSelectImage={this.onSelectImage}
-                />;});
+            return (
+                <Image
+                    key={"Image-" + idx + "-" + item.src}
+                    item={item}
+                    index={idx}
+                    margin={this.props.margin}
+                    height={this.props.rowHeight}
+                    isSelectable={this.props.enableImageSelection}
+                    onClick={this.getOnClickThumbnailFn()}
+                    onSelectImage={this.onSelectImage}
+                />
+            );
+        });
 
         return (
-                <div id="Gallery" ref={(c) => this._gallery = c}>
+            <div id="Gallery" ref={c => (this._gallery = c)}>
                 {images}
                 <Lightbox
-            images={this.props.images}
-            backdropClosesModal={this.props.backdropClosesModal}
-            currentImage={this.state.currentImage}
-            customControls={this.props.customControls}
-            enableKeyboardInput={this.props.enableKeyboardInput}
-            imageCountSeparator={this.props.imageCountSeparator}
-            isOpen={this.state.lightboxIsOpen}
-            onClickImage={this.getOnClickImageFn()}
-            onClickNext={this.getOnClickNextFn()}
-            onClickPrev={this.getOnClickPrevFn()}
-            showCloseButton={this.props.showCloseButton}
-            showImageCount={this.props.showImageCount}
-            onClose={this.closeLightbox}
-            width={this.props.lightboxWidth}
+                    images={this.props.images}
+                    backdropClosesModal={this.props.backdropClosesModal}
+                    currentImage={this.state.currentImage}
+                    customControls={this.props.customControls}
+                    enableKeyboardInput={this.props.enableKeyboardInput}
+                    imageCountSeparator={this.props.imageCountSeparator}
+                    isOpen={this.state.lightboxIsOpen}
+                    onClickImage={this.getOnClickImageFn()}
+                    onClickNext={this.getOnClickNextFn()}
+                    onClickPrev={this.getOnClickPrevFn()}
+                    showCloseButton={this.props.showCloseButton}
+                    showImageCount={this.props.showImageCount}
+                    onClose={this.closeLightbox}
+                    width={this.props.lightboxWidth}
                 />
-                </div>
+            </div>
         );
     }
 }
 
-Gallery.displayName = 'Gallery';
+Gallery.displayName = "Gallery";
 
 Gallery.propTypes = {
     images: PropTypes.arrayOf(
         PropTypes.shape({
             src: PropTypes.string.isRequired,
             thumbnail: PropTypes.string.isRequired,
+            embedToLightbox: PropTypes.bool.isRequired,
             srcset: PropTypes.array,
             caption: PropTypes.string,
             tags: PropTypes.arrayOf(
@@ -307,7 +308,7 @@ Gallery.defaultProps = {
     currentImage: 0,
     preloadNextImage: true,
     enableKeyboardInput: true,
-    imageCountSeparator: ' of ',
+    imageCountSeparator: " of ",
     isOpen: false,
     showCloseButton: true,
     showImageCount: true,
--- src/Image.js
+++ src/Image.js
@@ -1,9 +1,9 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import CheckButton from './CheckButton.js';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import CheckButton from "./CheckButton.js";
 
 class Image extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -11,11 +11,11 @@ class Image extends Component {
         };
     }
 
-    tileViewportStyle () {
+    tileViewportStyle() {
         if (this.props.item.isSelected)
             return {
-                width: this.props.item.vwidth -32,
-                height: this.props.height -32,
+                width: this.props.item.vwidth - 32,
+                height: this.props.height - 32,
                 margin: 16,
                 overflow: "hidden"
             };
@@ -26,27 +26,28 @@ class Image extends Component {
         };
     }
 
-    thumbnailStyle () {
-        if (this.props.item.isSelected){
-            var ratio = (this.props.item.scaletwidth / this.props.height);
+    thumbnailStyle() {
+        if (this.props.item.isSelected) {
+            var ratio = this.props.item.scaletwidth / this.props.height;
             var height = 0;
             var width = 0;
-            var viewportHeight = (this.props.height - 32);
-            var viewportWidth = (this.props.item.vwidth -32);
+            var viewportHeight = this.props.height - 32;
+            var viewportWidth = this.props.item.vwidth - 32;
 
-            if(this.props.item.scaletwidth > this.props.height){
-                width = this.props.item.scaletwidth -32;
+            if (this.props.item.scaletwidth > this.props.height) {
+                width = this.props.item.scaletwidth - 32;
                 height = Math.floor(width / ratio);
-            }
-            else {
-                height = this.props.height -32;
+            } else {
+                height = this.props.height - 32;
                 width = Math.floor(height * ratio);
             }
 
-            var marginTop = -Math.abs(Math.floor((viewportHeight - height) / 2));
+            var marginTop = -Math.abs(
+                Math.floor((viewportHeight - height) / 2)
+            );
             var marginLeft = -Math.abs(Math.floor((viewportWidth - width) / 2));
             return {
-                cursor: 'pointer',
+                cursor: "pointer",
                 width: width,
                 height: height,
                 marginLeft: marginLeft,
@@ -54,7 +55,7 @@ class Image extends Component {
             };
         }
         return {
-            cursor: 'pointer',
+            cursor: "pointer",
             width: this.props.item.scaletwidth,
             height: this.props.height,
             marginLeft: this.props.item.marginLeft,
@@ -62,112 +63,141 @@ class Image extends Component {
         };
     }
 
-    renderCheckButton () {
+    renderCheckButton() {
         return (
-                <CheckButton key="Select"
-            index={this.props.index}
-            color={"rgba(255, 255, 255, 0.7)"}
-            selectedColor={"#4285f4"}
-            hoverColor={"rgba(255, 255, 255, 1)"}
-            isSelected={this.props.item.isSelected}
-            isSelectable={this.props.isSelectable}
-            onClick={this.props.isSelectable ?
-                     this.props.onSelectImage : null}
-            parentHover={this.state.hover}/>
+            <CheckButton
+                key="Select"
+                index={this.props.index}
+                color={"rgba(0, 0, 0, 0.7)"}
+                selectedColor={"#F44336"}
+                hoverColor={"#4285f4"}
+                isSelected={this.props.item.isSelected}
+                isSelectable={this.props.isSelectable}
+                onClick={
+                    this.props.isSelectable ? this.props.onSelectImage : null
+                }
+                parentHover={this.state.hover}
+            />
         );
     }
 
-    render () {
-        var tags = (typeof this.props.item.tags === 'undefined') ? <noscript/> :
-                this.props.item.tags.map((tag) => {
-                    return <div title={tag.title}
-                    key={"tag-"+tag.value}
-                    style={{display: "inline-block",
-                            cursor: 'pointer',
-                            pointerEvents: 'visible',
-                            margin: "2px"}}>
-                        <span style={tagStyle}>{tag.value}</span>
-                        </div>;
-                });
+    render() {
+        var tags = typeof this.props.item.tags === "undefined"
+            ? <noscript />
+            : this.props.item.tags.map(tag => {
+                  return (
+                      <div
+                          title={tag.title}
+                          key={"tag-" + tag.value}
+                          style={{
+                              display: "inline-block",
+                              cursor: "pointer",
+                              pointerEvents: "visible",
+                              margin: "2px"
+                          }}
+                      >
+                          <span style={tagStyle}>{tag.value}</span>
+                      </div>
+                  );
+              });
 
-        var customOverlay = (typeof this.props.item.customOverlay === 'undefined')
-                ? <noscript/> :
-                <div style={{
-                    pointerEvents: "none",
-                    opacity: this.state.hover ? 1 : 0,
-                    position: "absolute",
-                    height: "100%",
-                    width: "100%"}}>
-                {this.props.item.customOverlay}
-            </div>;
+        var customOverlay = typeof this.props.item.customOverlay === "undefined"
+            ? <noscript />
+            : <div
+                  style={{
+                      pointerEvents: "none",
+                      opacity: this.state.hover ? 1 : 0,
+                      position: "absolute",
+                      height: "100%",
+                      width: "100%"
+                  }}
+              >
+                  {this.props.item.customOverlay}
+              </div>;
 
         return (
-                <div className="tile"
-            key={"tile-"+this.props.index}
-            onMouseEnter={(e) => this.setState({hover: true})}
-            onMouseLeave={(e) => this.setState({hover: false})}
-            style={{
-                margin: this.props.margin,
-                WebkitUserSelect: "none",
-                position: "relative",
-                float: "left",
-                background: "#eee",
-                padding: "0px"}}>
+            <div
+                className="tile"
+                key={"tile-" + this.props.index}
+                onMouseEnter={e => this.setState({ hover: true })}
+                onMouseLeave={e => this.setState({ hover: false })}
+                style={{
+                    margin: this.props.margin,
+                    WebkitUserSelect: "none",
+                    position: "relative",
+                    float: "left",
+                    background: "#eee",
+                    padding: "0px"
+                }}
+            >
 
-                <div className="tile-icon-bar"
-            key={"tile-icon-bar-"+this.props.index}
-            style={{
-                pointerEvents: "none",
-                opacity: 1,
-                position: "absolute",
-                height: "36px",
-                width: "100%"}}>
-                {this.renderCheckButton()}
+                <div
+                    className="tile-icon-bar"
+                    key={"tile-icon-bar-" + this.props.index}
+                    style={{
+                        pointerEvents: "none",
+                        opacity: 1,
+                        position: "absolute",
+                        height: "36px",
+                        width: "100%"
+                    }}
+                >
+                    {this.renderCheckButton()}
                 </div>
 
-                <div className="tile-bottom-bar"
-            key={"tile-bottom-bar-"+this.props.index}
-            style={{
-                padding: "2px",
-                pointerEvents: "none",
-                position: "absolute",
-                minHeight: "0px",
-                maxHeight: "160px",
-                width: "100%",
-                bottom: "0px"
-            }}>
-                {tags}
-            </div>
+                <div
+                    className="tile-bottom-bar"
+                    key={"tile-bottom-bar-" + this.props.index}
+                    style={{
+                        padding: "2px",
+                        pointerEvents: "none",
+                        position: "absolute",
+                        minHeight: "0px",
+                        maxHeight: "160px",
+                        width: "100%",
+                        bottom: "0px"
+                    }}
+                >
+                    {tags}
+                </div>
 
                 {customOverlay}
 
-                <div className="tile-overlay"
-            key={"tile-overlay-"+this.props.index}
-            style={{
-                pointerEvents: "none",
-                opacity: 1,
-                position: "absolute",
-                height: "100%",
-                width: "100%",
-                background: (this.state.hover
-                             && !this.props.item.isSelected
-                             && this.props.isSelectable) ?
-                    'linear-gradient(to bottom,rgba(0,0,0,0.26),transparent 56px,transparent)' : 'none'}}>
-                </div>
+                <div
+                    className="tile-overlay"
+                    key={"tile-overlay-" + this.props.index}
+                    style={{
+                        pointerEvents: "none",
+                        opacity: 1,
+                        position: "absolute",
+                        height: "100%",
+                        width: "100%",
+                        background: this.state.hover &&
+                            !this.props.item.isSelected &&
+                            this.props.isSelectable
+                            ? "linear-gradient(to bottom,rgba(0,0,0,0.26),transparent 56px,transparent)"
+                            : "none"
+                    }}
+                />
 
-                <div className="tile-viewport"
-            style={
-                this.tileViewportStyle()
-            }
-                key={"tile-viewport-"+this.props.index}
-            onClick={this.props.onClick ?
-                     (e) => this.props.onClick(this.props.index, e) : null}>
-                <img
-            key={"img-"+this.props.index}
-            src={this.props.item.thumbnail} title={this.props.item.caption}
-            style={this.thumbnailStyle()} />
+                <div
+                    className="tile-viewport"
+                    style={this.tileViewportStyle()}
+                    key={"tile-viewport-" + this.props.index}
+                    onClick={
+                        this.props.onClick
+                            ? e => this.props.onClick(this.props.index, e)
+                            : null
+                    }
+                >
+                    <img
+                        key={"img-" + this.props.index}
+                        src={this.props.item.thumbnail}
+                        title={this.props.item.caption}
+                        style={this.thumbnailStyle()}
+                    />
                 </div>
-                </div>
+            </div>
         );
     }
 }
